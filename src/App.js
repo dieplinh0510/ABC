521dd2c4c7a07238e8ca1f05f2db5005a029b1ac
Lam Linh <dieplinh1005.dh@gmail.com>
2023-09-23 16:42:16 +0700

    code buoi 7

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import CouterApp from "./component/counter/couterapp";
 import { allReducers } from "./component/counter/reducers/index";
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CouterAppV2, { CouterAppV3, CouterAppV4, CouterAppV5, CouterAppV6 } from "./component/counter-v2/CouterAppV2";
+import Login from "./component/react-form/Login";
+import Register from "./component/react-form/Register";
 
 const store = createStore( allReducers )
 
@@ -21,8 +24,14 @@ function App() {
 
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={ <TodoApp /> } />
-            <Route path="/counter" element={ <CouterApp /> } />
+            <Route path="/" element={ <Register /> } />
+            <Route path="/counter" element={ <CouterApp /> } >
+              <Route path="/counter/v2" index element={ <CouterAppV2 /> } />
+              <Route path="/counter/v3" index element={ <CouterAppV3 /> } />
+              <Route path="/counter/v4" index element={ <CouterAppV4 /> } />
+              <Route path="/counter/v5" index element={ <CouterAppV5 /> } />
+              <Route path="/counter/v6" index element={ <CouterAppV6 /> } />
+            </Route>
           </Routes>
         </BrowserRouter>
 
